f03e1d86f1660a94d22721609f63f64f4190362b
antonymendes <84051724+antonymendes@users.noreply.github.com>
2025-04-19 22:53:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function mostrarPagina(id) {
-  const todas = document.querySelectorAll('.slide');
-  todas.forEach(p => p.classList.add('hidden'));
-  document.getElementById(id).classList.remove('hidden');
-}
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('pagina1').classList.remove('hidden');
-});
